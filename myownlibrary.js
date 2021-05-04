bbec4f2aba02406ce3c1f7edc3d4bba57aca008b
anirudh-jaswal <79051994+anirudh-jaswal@users.noreply.github.com>
2021-05-04 22:14:07 +0530

    Add files via upload

---
+++ myownlibrary.js
@@ -0,0 +1,9 @@
+function hasCollided(Lbullet,Lwall){
+    bulletRightEdge=bullet.x +bullet.width
+    wallLeftEdge=wall.x
+    if (bulletRightEdge>=wallLeftEdge)
+    {
+      return true
+    }
+    return false
+  }
